f7109ca9009680d9bd3da84bded1c9776b54aa42
shikokumid <orgaadaiman@gmail.com>
2026-05-28 00:43:11 +0700

    new dbcon

--- order_details.php
+++ order_details.php
@@ -17,7 +17,8 @@ $order_id = $_GET['id'];
 $username = $_COOKIE['login'];
 
 // Подключение к базе
-$pdo = new PDO('mysql:host=localhost;dbname=diplom;port=3306', 'root', '');
+$pdo = new PDO('mysql:host=mysql-so2r.railway.internal;dbname=railway;port=3306', 'root', 'zUuofgBLCodqyylBPVacalWLUzyDmyhs');
+ 
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 // Получаем информацию о заказе
