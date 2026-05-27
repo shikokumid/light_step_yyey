4531f6011d4c82271da729de5b8f63a79f2e4a9c
shikokumid <orgaadaiman@gmail.com>
2026-05-28 00:42:49 +0700

    new

--- getabout.php
+++ getabout.php
@@ -4,8 +4,8 @@ if(strlen($email) < 2 && !str_contains($email,'@')){
     echo'error';
     exit;
 }
-$pdo = new PDO('mysql:host=localhost;dbname=diplom;port=3306', 'root', '');
-
+$pdo = new PDO('mysql:host=mysql-so2r.railway.internal;dbname=railway;port=3306', 'root', 'zUuofgBLCodqyylBPVacalWLUzyDmyhs');
+ 
 $sql = 'INSERT INTO justemail(EMAIL) VALUES(?)';
 $query = $pdo->prepare($sql);
 $query->execute([$email]);
