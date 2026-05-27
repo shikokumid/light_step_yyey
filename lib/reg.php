65b312cd7ab625475f332188e2fbd48b977ab590
shikokumid <orgaadaiman@gmail.com>
2026-05-28 00:39:53 +0700

    new dbcon

--- lib/reg.php
+++ lib/reg.php
@@ -15,8 +15,8 @@ if(strlen($email) < 2 && !str_contains($email,'@')){
 $hash_password = "lasdl;askd;ak213";
 $password = md5($hash_password.$password);
 // date base
-$pdo = new PDO('mysql:host=localhost;dbname=diplom;port=3306', 'root', '');
-// insert
+$pdo = new PDO('mysql:host=mysql-so2r.railway.internal;dbname=railway;port=3306', 'root', 'zUuofgBLCodqyylBPVacalWLUzyDmyhs');
+ // insert
 $sql = 'INSERT INTO regisrtation(NAME, MAIL, PASSWORD) VALUES(?,?,?)';
 $query = $pdo->prepare($sql); 
 $query ->execute([$login, $email, $password]);
