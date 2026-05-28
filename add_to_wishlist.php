238f1079ee4dcbbf894cc0f46f27876920687c39
shikokumid <orgaadaiman@gmail.com>
2026-05-28 15:38:20 +0700

    new dbcon

--- add_to_wishlist.php
+++ add_to_wishlist.php
@@ -10,7 +10,7 @@ if (!isset($_COOKIE['login'])) {
 $username = $_COOKIE['login'];
 
 // Подключение к БД
-$pdo = new PDO('mysql:host=localhost;dbname=diplom;port=3306', 'root', '');
+$pdo = new PDO('mysql:host=mysql-so2r.railway.internal;dbname=railway;port=3306', 'root', 'zUuofgBLCodqyylBPVacalWLUzyDmyhs');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 // Получаем ID пользователя
