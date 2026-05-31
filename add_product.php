30a8108a4d8e91c2944903d5361b8436c7b03e4f
shikokumid <orgaadaiman@gmail.com>
2026-05-31 23:42:31 +0700

    Update add_product.php

--- add_product.php
+++ add_product.php
@@ -1,24 +1,85 @@
 <?php
+session_start();
 
 $pdo = new PDO(
-'mysql:host=mysql-so2r.railway.internal;dbname=railway;port=3306',
-'root',
-'zUuofgBLCodqyylBPVacalWLUzyDmyhs'
+    'mysql:host=mysql-so2r.railway.internal;dbname=railway;port=3306',
+    'root',
+    'zUuofgBLCodqyylBPVacalWLUzyDmyhs'
 );
 
-$item = $_POST['item'];
-$price = $_POST['price'];
-$image = $_POST['image'];
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$item = trim($_POST['item']);
+$price = trim($_POST['price']);
+
+if (!$item || !$price) {
+    die('Заполните все поля');
+}
+
+
+
+if (!isset($_FILES['image'])) {
+    die('Фото не выбрано');
+}
+
+$image = $_FILES['image'];
+
+if ($image['error'] !== 0) {
+    die('Ошибка загрузки изображения');
+}
+
+$allowed = ['jpg', 'jpeg', 'png', 'webp'];
+
+$ext = strtolower(
+    pathinfo($image['name'], PATHINFO_EXTENSION)
+);
+
+if (!in_array($ext, $allowed)) {
+    die('Допустимы только JPG PNG WEBP');
+}
+
+/*
+|--------------------------------------------------------------------------
+| Уникальное имя
+|--------------------------------------------------------------------------
+*/
+
+$fileName =
+    uniqid() .
+    '.' .
+    $ext;
+
+/*
+|--------------------------------------------------------------------------
+| Папка images
+|--------------------------------------------------------------------------
+*/
+
+$uploadPath =
+    __DIR__ .
+    '/images/' .
+    $fileName;
+
+move_uploaded_file(
+    $image['tmp_name'],
+    $uploadPath
+);
 
 $stmt = $pdo->prepare("
-INSERT INTO products(item, price, image)
-VALUES(?,?,?)
+    INSERT INTO products (
+        item,
+        price,
+        image
+    )
+    VALUES (?, ?, ?)
 ");
 
 $stmt->execute([
     $item,
     $price,
-    $image
+    $fileName
 ]);
 
 header('Location: admin.php');
+exit;
+?>
