76445ce96b8ff4855189bc6264445132ec8448ca
shikokumid <orgaadaiman@gmail.com>
2026-05-31 22:17:44 +0700

    Update auth.php

--- lib/auth.php
+++ lib/auth.php
@@ -12,16 +12,12 @@ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $login = trim($_POST['login'] ?? '');
 $password = $_POST['password'] ?? '';
 
-if (strlen($login) < 2) {
-    exit('Введите логин');
-}
-
-if (strlen($password) < 1) {
-    exit('Введите пароль');
+if (!$login || !$password) {
+    exit('Введите логин и пароль');
 }
 
 $stmt = $pdo->prepare("
-    SELECT id, NAME, PASSWORD
+    SELECT *
     FROM regisrtation
     WHERE NAME = ?
     LIMIT 1
@@ -35,16 +31,46 @@ if (!$user) {
     exit('Неверный логин или пароль');
 }
 
-if (!password_verify($password, $user['PASSWORD'])) {
-    exit('Неверный логин или пароль');
+$dbPassword = $user['PASSWORD'];
+
+/*
+Поддержка новых паролей password_hash()
+*/
+if (password_verify($password, $dbPassword)) {
+
+    setcookie('login', $user['NAME'], time() + 60*60*24*30, '/');
+
+    header('Location: /user.php');
+    exit;
 }
 
-setcookie(
-    'login',
-    $user['NAME'],
-    time() + 60 * 60 * 24 * 30,
-    '/'
-);
+/*
+Поддержка старых md5
+*/
+$oldSalt = 'lasdl;askd;ak213';
 
-header('Location: /user.php');
-exit;
+if (md5($oldSalt . $password) === $dbPassword) {
+
+    /*
+    Сразу обновляем пароль на password_hash
+    */
+    $newHash = password_hash($password, PASSWORD_DEFAULT);
+
+    $update = $pdo->prepare("
+        UPDATE regisrtation
+        SET PASSWORD = ?
+        WHERE id = ?
+    ");
+
+    $update->execute([
+        $newHash,
+        $user['id']
+    ]);
+
+    setcookie('login', $user['NAME'], time() + 60*60*24*30, '/');
+
+    header('Location: /user.php');
+    exit;
+}
+
+exit('Неверный логин или пароль');
