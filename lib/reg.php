4359dff58dc7c97aff3c56b5a4ced50cbeddd0c4
shikokumid <orgaadaiman@gmail.com>
2026-05-31 19:37:01 +0700

    fix bags#1

--- lib/reg.php
+++ lib/reg.php
@@ -1,7 +1,6 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-require 'vendor/autoload.php';
 $login = trim($_POST['login'] ?? '');
 $email = trim($_POST['email'] ?? '');
 $password = $_POST['password'] ?? '';
